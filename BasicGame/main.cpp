61eb63e6be2f681f942d8e3ce85ede0bb90c4444
Ida Svensson <hello@idasvensson.com>
2013-12-05 21:46:28 +0100

    Initial Commit

---
+++ BasicGame/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BasicGame
+//
+//  Created by Ida Svensson on 2013-12-05.
+//  Copyright (c) 2013 Ida Svensson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
